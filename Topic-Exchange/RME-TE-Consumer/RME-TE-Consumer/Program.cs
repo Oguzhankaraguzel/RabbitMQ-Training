86b499b8cfda40a1d068ba988dd569b6cb547221
Oguzhankaraguzel <oguzhan.karaguzel@gmail.com>
2023-09-03 01:56:12 +0300

    Topic exchange

---
+++ Topic-Exchange/RME-TE-Consumer/RME-TE-Consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+
+/*
+ * try to topic like this 
+ * *.hello.*
+ * *.hello
+ * #.world.#
+ * world.#
+ * *.*.world
+ * #.hello
+ */
+
+Console.WriteLine("write topic");
+string topic = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example",
+    routingKey: topic
+    );
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    string messages = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(messages);
+};
+
+Console.Read();
---
+++ Topic-Exchange/RMQ-TE-Publisher/RMQ-TE-Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic
+    );
+
+/*
+ * try to topic like this 
+ * *.hello.*
+ * *.hello
+ * #.world.#
+ * world.#
+ * *.*.world
+ * #.hello
+ */
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i}");
+    Console.WriteLine("Write topic");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+        exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message
+        );
+}
+
+Console.Read();
