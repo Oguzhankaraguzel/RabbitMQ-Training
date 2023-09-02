6222f85c1065e0af8d2990bb14d1bcaa3a0d30d6
Oguzhankaraguzel <oguzhan.karaguzel@gmail.com>
2023-09-03 02:47:34 +0300

    Header Exchange

---
+++ Header Exchange/RMQ-HE-Consumer/RMQ-HE-Consumer/Program.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+/*
+ * xmatch : ilgili queue'nun mesajı hangi davranışla alacağının kararını veren bir parametredir
+ * 
+ * xmatch : it is a parameter that decides the behavior of the corresponding queue to receive the message.
+ * 
+ * xmatch : any
+ * 
+ * ilgili queue'nun sadece tek bir key-value eşleşmesi durumunda mesajı alır.
+ *
+ * receives the message if only one key-value match of the corresponding queue.
+ * 
+ * xmatch : all
+ * 
+ * mesajı almak için bütün key-value değerleri eşleşmek zorunda
+ * 
+ * All key-values have to match to get the message
+ */
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+Console.WriteLine("Please write the value");
+string value = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange-example",
+    routingKey: string.Empty,
+    arguments: new Dictionary<string, object>
+    {
+        ["no"] = value
+    }
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Header Exchange/RMQ-HE-Publisher/RMQ-HE-Publisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+/*
+ * xmatch : ilgili queue'nun mesajı hangi davranışla alacağının kararını veren bir parametredir
+ * 
+ * xmatch : it is a parameter that decides the behavior of the corresponding queue to receive the message.
+ * 
+ * xmatch : any
+ * 
+ * ilgili queue'nun sadece tek bir key-value eşleşmesi durumunda mesajı alır.
+ *
+ * receives the message if only one key-value match of the corresponding queue.
+ * 
+ * xmatch : all
+ * 
+ * mesajı almak için bütün key-value değerleri eşleşmek zorunda
+ * 
+ * All key-values have to match to get the message
+ */
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i}");
+    Console.WriteLine("Please write the value");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>()
+    {
+        ["no"] = value
+    };
+
+    channel.BasicPublish(
+        exchange: "header-exchange-example",
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties
+        );
+}
+
+Console.Read();
