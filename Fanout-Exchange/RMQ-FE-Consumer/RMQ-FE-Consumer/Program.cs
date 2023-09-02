765aecee95d1bb261db1a24c89236250d60230a3
Oguzhankaraguzel <oguzhan.karaguzel@gmail.com>
2023-09-03 00:33:10 +0300

    fannout exchange

---
+++ Fanout-Exchange/RMQ-FE-Consumer/RMQ-FE-Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout
+    );
+
+Console.WriteLine("Write the queue name");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: queueName,
+    exclusive: false
+    );
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "fanout-exchange-example",
+    routingKey: string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Fanout-Exchange/RMQ-FE-Publisher/RMQ-FE-Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout
+    );
+
+for (int i = 100; i <= 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i}");
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        /*
+         * routing key boş bırakılarak, bu exchange'e bağlanmış her consumer'a, kuyruğa bakılmaksızın mesaj gönderilir.
+         * By leaving the routing key blank, a message is sent to every consumer connected to this exchange, regardless of the queue.
+         */
+        routingKey: string.Empty,
+        body: message
+        );
+}
+
+Console.Read();
