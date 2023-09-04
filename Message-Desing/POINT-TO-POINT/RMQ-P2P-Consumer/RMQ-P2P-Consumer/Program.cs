b67338fc4f234d48384c42672523547e68bee083
Oguzhankaraguzel <oguzhan.karaguzel@gmail.com>
2023-09-04 21:35:24 +0300

    P2P

---
+++ Message-Desing/POINT-TO-POINT/RMQ-P2P-Consumer/RMQ-P2P-Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+/*
+ * point to point tasarımında genellikle direct exchange kullanılır.
+ * In point-to-point design, direct exchange is often used.
+ */
+
+string queueName = "example-p2p-queue";
+
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Message-Desing/POINT-TO-POINT/RMQ-P2P-Publisher/RMQ-P2P-Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://jizvoypt:baU73GaP3L0Iyi17yRGXndIhBA_1LT-F@sparrow.rmq.cloudamqp.com/jizvoypt");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+/*
+ * point to point tasarımında genellikle direct exchange kullanılır.
+ * In point-to-point design, direct exchange is often used.
+ */
+
+string queueName = "example-p2p-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+    );
+
+byte[] message = Encoding.UTF8.GetBytes("Hello");
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: queueName,
+    body: message
+    );
+
+Console.Read();
